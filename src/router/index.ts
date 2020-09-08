1a19fac90704e27b4ef49e4896f995e173d8eccf
leijin0416 <1427780404@qq.com>
2020-09-09 00:06:50 +0800

    router路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,17 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+import { createRouter, createWebHistory } from "vue-router";
+import { routerMaps } from './routerMap';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: routerMaps,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    next();
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ src/router/routerMap.ts
@@ -0,0 +1,39 @@
+import { RouteRecordRaw } from "vue-router";
+
+/**
+ * getComponentMin() 路由懒加载
+ * @param {string} name  -文件夹
+ * @param {string} component  -.vue
+ */
+const getComponentMin = (component: any) => () =>
+  import(`@/views/${component}.vue`);
+
+const getComponentMax = (name: any, component: any) => () =>
+  import(`@/views/${name}/${component}.vue`);
+
+/**
+ * @param {boolean} displayNavBar 是否需要底部导航
+ * @param {boolean} keepAlive 是否缓存页面
+ * @param {string}  title 页面标题
+ * 
+ * @param {string}  redirect 路由重定向
+ * to.meta.index || from.meta.index || this.$router.isBack
+ */
+export const routerMaps: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/index',
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: getComponentMin('About'),
+    meta: { title: 'NAI Trading Center', displayNavBar: false }
+  },
+  {
+    path: '/index',
+    name: 'Home',
+    component: getComponentMin('Home'),
+    meta: { title: 'NAI Trading Center', displayNavBar: false }
+  },
+]
