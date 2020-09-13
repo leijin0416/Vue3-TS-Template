d7d1c28249a33e306458339e2821175c2a65b459
leijin0416 <1427780404@qq.com>
2020-09-14 00:16:26 +0800

    公共请求管理

--- src/api/common/index.ts
+++ src/api/common/index.ts
@@ -7,4 +7,4 @@ import Request from '@/api/axios/http'
  * 短信区号
  * @param {*} params 
  */
-export const webGetSelectAreaCode = (params: any) => Request.post('/api/areaCode/selectAreaCode', params);
+export const webGetSelectAreaCode = (params: any) => Request.post('/api/areaCode/selectAreaCode', params)
