61dcac8fa07b2a3b374fdc351351b0b237069797
leijin0416 <1427780404@qq.com>
2020-09-16 00:15:00 +0800

    vuex状态管理

--- src/store/modules/storageUser.ts
+++ src/store/modules/storageUser.ts
@@ -4,14 +4,20 @@ export default {
   namespaced: true,
   state: {
     getSessionUserToken: '001',
+    getSessionNavTabrsID: '',
   },
   getters: {
     getSessionUserToken: (state: any) => state.getSessionUserToken,
+    getSessionNavTabrsID: (state: any) => state.getSessionNavTabrsID,
   },
   mutations: {
     SET_sessionUserToken(state: any, userToken: any) {
       state.getSessionUserToken = userToken
       sessionData('set', 'getSessionUserToken', userToken)
+    },
+    SET_sessionNavTabrsID(state: any, context: any) {
+      state.getSessionNavTabrsID = context
+      sessionData('set', 'getSessionNavTabrsID', context)
     },
   },
   actions: {},
