2c840f26e6e0499291a7400b1e029e860c0e4c23
leijin0416 <1427780404@qq.com>
2020-09-16 00:11:14 +0800

    底部导航组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="create-app">
     <router-view v-wechat-title="$route.meta.title" />
+    <MTabBar 
+      v-if="$route.meta.displayNavBar"
+      :navTabsData="navTabsData"
+      @onTabsChange="onTabsChange"></MTabBar>
   </div>
 </template>
+
+<script lang="ts">
+import { reactive, onMounted, computed, toRefs, watch } from "vue"
+import MTabBar from "@/components/navTabsBottom/index.vue"
+
+export default {
+  components: {
+    MTabBar,
+  },
+  setup() {
+    const state = reactive({
+      navTabsData: [
+        {label: '首页', name: 1, active: require('@/assets/images/nav-otc-inactive.png'), inactive: require('@/assets/images/nav-otc-active.png'), path: '/'},
+        {label: '我的', name: 2, active: require('@/assets/images/nav-home-inactive.png'), inactive: require('@/assets/images/nav-home-active.png'), path: '/about'},
+      ],
+    })
+
+    const onTabsChange = (index: any) => {
+      // console.log(index);
+    }
+
+    return {
+      ...toRefs(state),
+      onTabsChange
+    }
+  },
+}
+</script>
 
 <style lang="scss">
 // 谷歌字体
---
+++ src/components/navTabsBottom/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="v-navtabs-box">
+    <van-tabbar 
+      v-model="active" 
+      @change="onTabsChange"
+      :placeholder="true"
+      :border="true"
+      z-index="9999"
+      fixed >
+      <van-tabbar-item 
+        v-for="(item, index) of navTabsData"  
+        :key="item.name"
+        :to="item.path" 
+        :name="item.name">
+        <span>{{item.label + active}}</span>
+        <template #icon="props">
+            <img :src="props.active ? item.active : item.inactive" />
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, ref, onMounted, computed, toRefs, watch } from "vue"
+import { useRouter, useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+import { sessionData } from '@/filters/storage'
+
+export default {
+  props: {
+    navTabsData: Array,
+  },
+  setup(props:any, context:any) {
+    // console.log(props.navTabsData)
+    // ctx.emit('event')
+    const router = useRouter()   // 路由
+    const route = useRoute()    // 获取路由信息
+    const store = useStore() // 状态管理vuex
+
+    const state = reactive({
+      id: 1,
+    })
+    const active = ref(1)
+
+    /**
+     *  监听vuex
+     */
+    watch(() => store.state.storageUser.getSessionNavTabrsID, (newer, older) => {
+      if (newer == null) {
+        return
+      } else {
+        active.value = newer
+        console.log(`navTab newer is ${newer}`)
+      }
+    }, { deep: true })
+
+    onMounted(async () => {
+      const navId = store.getters["storageUser/getSessionNavTabrsID"]
+      const sessionTabs = sessionData("get", "getSessionNavTabrsID", "")
+      // console.log(sessionTabs);
+
+      if (navId === '' && sessionTabs === null) {
+        // 第一次进来的时候
+        active.value = 1;
+
+      } else if (navId === '' && sessionTabs !== null) {
+        // 刷新页面的时候
+        let displayNavBar = route.meta.displayNavBar;
+        if (displayNavBar) {
+          if (sessionTabs === '1') router.push('/');
+          if (sessionTabs === '2') router.push('/about');
+        }
+        // Reflect.set(active, 0, Number(sessionTabs));
+        active.value = Number(sessionTabs);
+        
+      } else {
+        // 默认
+        active.value = Number(navId);
+      }
+    })
+
+    const onTabsChange = (index: any) => {
+      store.commit('storageUser/SET_sessionNavTabrsID', index)
+      context.emit('onTabsChange', index)
+    }
+
+    return {
+      ...toRefs(state),
+      active,
+      onTabsChange
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+/deep/.van-tabbar-item--active {
+  color: red;
+}
+/deep/.van-tabbar-item__icon img {
+  width: auto;
+  height: 20px;
+}
+/deep/.van-tabbar {
+  background-color: #f1f1f1;
+}
+</style>
