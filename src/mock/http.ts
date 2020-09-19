cc905c1c7b8a5cb2e53d7278789480e2416ca7b4
leijin0416 <1427780404@qq.com>
2020-09-20 00:28:47 +0800

    mock第三方数据

--- src/mock/http.ts
+++ src/mock/http.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios"
 import { Toast } from "vant"
 
-axios.defaults.baseURL = ''
+axios.defaults.baseURL = ""
 /**
  * 提示函数
  * 禁止点击蒙层、显示一秒后关闭 vant.Toast('提示');
@@ -32,24 +32,24 @@ axios.interceptors.response.use(response => {
   if (error && error.response) {
     switch (status) {
       case 400:
-        tip('小主~ 我们请求出错')
+        tip("小主~ 我们请求出错")
         break
       case 401:
-        tip('系统提示：您未登录')
+        tip("系统提示：您未登录")
         return
       case 403:
-        tip('登录过期，请重新登录')
+        tip("登录过期，请重新登录")
         break
       case 404:
-        tip('系统提示：请求资源未找到')
+        tip("系统提示：请求资源未找到")
         break
       case 500:
-        tip('系统提示：服务端出错')
+        tip("系统提示：服务端出错")
         break
     }
     return status >= 200 && status < 300
   } else {
-      tip('小主~ 网络开小差，稍后再试')
+    tip("小主~ 网络开小差，稍后再试")
   }
   return Promise.reject(error.response)
 })
@@ -58,7 +58,7 @@ export default {
   get(url: any, param: any) {
     return new Promise((resolve, reject) => {
       axios({
-        method: 'get',
+        method: "get",
         url,
         params: param
       })
@@ -73,7 +73,7 @@ export default {
   post(url: any, param: any) {
     return new Promise((resolve, reject) => {
       axios({
-        method: 'post',
+        method: "post",
         url,
         data: param
       })
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,15 +1,15 @@
 /**
  * 请求统一管理
  */
-import requestMock from './http'
+import requestMock from "./http"
 
 /**
  * 新闻
  * @param {*} params 
  */
-export const webGetInewNewsPages = (params: any) => requestMock.get('/inew/news/get', params)
+export const webGetInewNewsPages = (params: any) => requestMock.get("/inew/news/get", params)
 /**
  * 新闻Title
  * @param {*} params 
  */
-export const webGetInewNewsChannel = (params: any) => requestMock.get('/inew/news/channel', params)
+export const webGetInewNewsChannel = (params: any) => requestMock.get("/inew/news/channel", params)
