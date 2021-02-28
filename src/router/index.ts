e017237409c0db7bf9a7845285fa507b14bc50fd
leijin0416 <1427780404@qq.com>
2021-02-28 22:52:51 +0800

    router：路由管理

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,38 +16,38 @@ history.removeItem("count")
 
 router.beforeEach((to, from, next) => {
   // 第一级栏目
-  const subArr = ["/", "/index", "/home"]
+  const subArr = ['/', '/index', '/home']
   const [toIndex, prevIndex] = [history.getItem(to.path), history.getItem(from.path)]
-  for (let index in subArr) history.setItem(subArr[index], "0")
+  for (let index in subArr) history.setItem(subArr[index], '0')
   const numToIndex = Number(toIndex)  // 当前
   const numPrevIndex = Number(prevIndex) // 过去的
 
-  if (to.path === "/login") {
+  if (to.path === '/login') {
     next()
   } else {
     // console.log(numToIndex)
     // console.log(numPrevIndex)
-
     if (!subArr.includes(to.path)) {
       ++numCount
       history.setItem(to.path, numCount.toString())
-      store.commit("vuexStorageNav/SET_sessionNavTabrsType", "next")
+      store.commit('vuexStorageNav/SET_sessionNavTabrsType', 'next')
       
     } else if (numToIndex || numToIndex === 0) {
       if (numToIndex > numPrevIndex) {
-        console.log("===== NAV TYPE STRAT =====")
-        store.commit("vuexStorageNav/SET_sessionNavTabrsType", "next")
+        console.log('=== Router_NAVid_next_STRAT ===')
+        store.commit('vuexStorageNav/SET_sessionNavTabrsType', 'next')
 
-      } else if (numToIndex === numPrevIndex) {  // 一级栏目无切换效果
-        console.log("===== NAV TYPE END =====")
-        sessionData("clean", "getSessionNavTabrsType", "")
-        store.commit("vuexStorageNav/SET_sessionNavTabrsType", "null")
+      } else if (numToIndex === numPrevIndex) {  
+        // 一级栏目无切换效果
+        console.log('=== Router_NAVid_HOME_END ===')
+        sessionData('clean', 'getSessionNavTabrsType', '')
+        store.commit('vuexStorageNav/SET_sessionNavTabrsType', 'null')
 
       } else {
-        console.log("===== NAV TYPE STRAT =====")
-        store.commit("vuexStorageNav/SET_sessionNavTabrsType", "prev")
+        console.log('=== Router_NAVid_prev_STRAT ===')
+        store.commit('vuexStorageNav/SET_sessionNavTabrsType', 'prev')
       }
-    } else {console.log("===== NAV TYPE END =====")}
+    } else {console.log('=== Router_NAVid_TYPE_Null ===')}
     next()
   }
 })
